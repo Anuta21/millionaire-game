af6509f761632e210417f40a9031c45975c2d027
Anuta21 <anuta.200214@gmail.com>
2023-06-12 23:56:12 +0300

    gh-pages

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { GameStartPage, GameOverPage, QuestionsPage } from './pages';
 
 export const App: React.FC = () => (
